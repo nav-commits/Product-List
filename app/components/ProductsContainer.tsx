65f5a5e8cdeb25747c5c3bcec2e310184801f8b6
nav-commits <53131408+nav-commits@users.noreply.github.com>
2025-08-26 14:56:55 -0400

    first commit

---
+++ app/components/ProductsContainer.tsx
@@ -0,0 +1,10 @@
+export default function ProductsContainer() {
+  return (
+    <div className="flex flex-col px-4">
+      <h1 className="text-4xl font-bold mb-4">Desserts</h1>
+      <p className="text-lg text-gray-700">
+        This is centered content using Flexbox
+      </p>
+    </div>
+  );
+}
